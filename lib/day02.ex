e43fbc1f8be4d4c9e6ca69b2c277b8b7fca26c54
Staszek Pasko <staszek@gmail.com>
2021-12-02 12:06:50 +0100

    a bit more concise

--- lib/day02.ex
+++ lib/day02.ex
@@ -14,33 +14,24 @@
 
 defmodule Aoc2021.Day02 do
 
-  def process1([dir, val], {x, d}) do
-    v = String.to_integer(val)
-    case dir do
-      "forward" -> { x + v, d }
-      "down" -> { x, d + v }
-      "up" -> { x, d - v }
-    end
+  def process(args) do
+    args |> Enum.map(&String.split/1)
+         |> Enum.map(fn [dir, val] -> {String.to_atom(dir), String.to_integer(val)} end)
   end
+
+  def move1({:forward, v}, {x, d}), do: { x + v, d }
+  def move1({:down, v}, {x, d}), do: { x, d + v}
+  def move1({:up, v}, {x, d}), do: { x, d - v}
 
   def part1(args) do
-    args |> Enum.map(&String.split/1)
-         |> Enum.reduce({0, 0}, fn it, acc -> process1(it,acc) end)
-         |> (fn { x , d } -> x * d end).()
+    process(args) |> Enum.reduce({0, 0}, &move1/2) |> (fn { x , d } -> x * d end).()
   end
 
-  def process2([dir, val], {x, d, a}) do
-    v = String.to_integer(val)
-    case dir do
-      "forward" -> { x + v, d + a * v, a}
-      "down" -> { x, d, a + v }
-      "up" -> { x, d, a - v }
-    end
-  end
+  def move2({:forward, v}, {x, d, a}), do: { x + v, d + a * v, a }
+  def move2({:down, v}, {x, d, a}), do: { x, d, a + v }
+  def move2({:up, v}, {x, d, a}), do: { x, d, a - v }
 
   def part2(args) do
-    args |> Enum.map(&String.split/1)
-         |> Enum.reduce({0, 0, 0}, fn it, acc -> process2(it,acc) end)
-         |> (fn { x , d, _ } -> x * d end).()
+    process(args) |> Enum.reduce({0, 0, 0}, &move2/2) |> (fn { x , d, _ } -> x * d end).()
   end
 end
