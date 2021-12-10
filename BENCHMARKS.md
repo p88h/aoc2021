7835cf6bec10fa8a7029acc1b38903851c350d97
Staszek Pasko <staszek@gmail.com>
2021-12-10 18:28:08 +0100

    some more runtime args handling + benchmark results

---
+++ BENCHMARKS.md
@@ -0,0 +1,29 @@
+Compiling 1 file (.ex)
+Operating System: Linux
+CPU Information: Intel(R) Core(TM) i7-8086K CPU @ 4.00GHz
+Number of Available Cores: 12
+Available memory: 15.59 GB
+Elixir 1.12.3
+Erlang 24.1.4
+
+Name                    ips        average  deviation         median         99th %
+Day 01 Part 1       9631.30       0.104 ms    ±30.92%      0.0820 ms       0.166 ms
+Day 01 Part 2       2988.57        0.33 ms    ±39.13%        0.27 ms        0.78 ms
+Day 02 Part 1        148.52        6.73 ms    ±43.10%        5.52 ms       22.85 ms
+Day 02 Part 2        146.07        6.85 ms    ±23.99%        6.23 ms        9.52 ms
+Day 03 Part 1        582.38        1.72 ms    ±28.85%        1.51 ms        2.96 ms
+Day 03 Part 2        728.96        1.37 ms    ±32.66%        1.11 ms        2.47 ms
+Day 04 Part 1        181.69        5.50 ms    ±23.15%        4.68 ms        7.63 ms
+Day 04 Part 2        173.95        5.75 ms    ±23.08%        5.01 ms        8.30 ms
+Day 05 Part 1          9.93      100.74 ms    ±10.58%       99.84 ms      134.80 ms
+Day 05 Part 2          5.15      194.01 ms    ±11.28%      195.57 ms      234.84 ms
+Day 06 Part 1      27092.38      0.0369 ms    ±32.82%      0.0299 ms      0.0662 ms
+Day 06 Part 2      18706.84      0.0535 ms    ±38.04%      0.0437 ms       0.101 ms
+Day 07 Part 1       3525.36        0.28 ms    ±26.44%        0.24 ms        0.43 ms
+Day 07 Part 2       2916.21        0.34 ms    ±26.26%        0.29 ms        0.51 ms
+Day 08 Part 1         65.58       15.25 ms   ±130.36%        7.18 ms       84.09 ms
+Day 08 Part 2         22.52       44.41 ms    ±16.54%       44.11 ms       61.89 ms
+Day 09 Part 1        167.48        5.97 ms    ±25.33%        5.22 ms        9.36 ms
+Day 09 Part 2         30.46       32.83 ms    ±14.44%       32.35 ms       45.59 ms
+Day 10 Part 1        693.75        1.44 ms    ±29.56%        1.25 ms        2.51 ms
+Day 10 Part 2        670.38        1.49 ms    ±32.12%        1.29 ms        2.71 ms
--- lib/main.ex
+++ lib/main.ex
@@ -12,33 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-defmodule Mix.Tasks.Main do
+ defmodule Mix.Tasks.Main do
+  def file_to_day(p), do: Path.basename(p) |> Path.rootname() |> String.slice(-2,2)
+  def all_days(), do: Path.wildcard("input/day*.txt") |> Enum.map(&file_to_day/1)
+
+  def parse_args([], cfg = %{days: []}), do: %{cfg | days: [ List.last(all_days()) ] }
+  def parse_args([], cfg), do: cfg
+  def parse_args(["bench" | t], cfg), do: parse_args(t, %{cfg | bench: true })
+  def parse_args(["all" | t], cfg), do: parse_args(t, %{cfg | days: all_days() })
+  def parse_args([ s | t ], cfg), do: parse_args(t, %{cfg | days: [ s | cfg[:days]] })
+
   use Mix.Task
   @shortdoc "Advent of Code Runner"
   @spec run(any) :: any
   def run(args) do
-    day = if length(args) > 0 do
-      # Use provided day number (needs to be 0-prefixed just like modules and files are)
-      hd(args)
+    config = parse_args(args, %{bench: false, days: []})
+
+    runnables = for day <- config[:days], into: [] do
+      module = String.to_existing_atom("Elixir.Aoc2021.Day#{day}")
+      file = File.open!("input/day#{day}.txt", [:read, :utf8])
+      input = IO.read(file, :all) |> String.split("\n")
+      [ { "Day #{day} Part 1",  fn -> module.part1(input) end },
+        { "Day #{day} Part 2",  fn -> module.part2(input) end } ]
+    end |> List.flatten() |> Enum.into(%{})
+
+    if config[:bench] do
+      Benchee.run(runnables, warmup: 1, time: 3, parallel: 6 )
     else
-      # Figure out the last day based on input files
-      tl(Path.wildcard("input/day*.txt")) |> Path.basename() |> Path.rootname() |> String.slice(-2,2)
+      Enum.each(runnables, fn {name, fun} -> fun.() |> IO.inspect(label: name) end)
     end
-
-    module = String.to_existing_atom("Elixir.Aoc2021.Day#{day}")
-    file = File.open!("input/day#{day}.txt", [:read, :utf8])
-    input = IO.read(file, :all) |> String.split("\n")
-
-    Benchee.run(
-      %{
-        "Day #{day} Part 1" => fn -> module.part1(input) end,
-        "Day #{day} Part 2" => fn -> module.part2(input) end
-      },
-      warmup: 1,
-      time: 3
-    )
-
-    module.part1(input) |> IO.inspect(label: "Day #{day} Part 1 result:")
-    module.part2(input) |> IO.inspect(label: "Day #{day} Part 2 result:")
   end
 end
