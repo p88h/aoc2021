68ca0ec73d7306d28a466f6d70a30323ce05f1f1
Staszek Pasko <staszek@gmail.com>
2021-12-13 14:55:33 +0100

    Day 13 vis

---
+++ vis/day13.py
@@ -0,0 +1,177 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pygame
+import os
+import math
+from common import View, Controller
+from pygame import gfxdraw as gd
+
+class Point:
+    def __init__(self, pos):
+        self.pos = pos
+        self.target = pos
+        self.sx = 1
+        self.sy = 1
+        self.ms = 1
+        self.maxs = 1
+
+    def update(self, view, base):
+        (x, y) = self.pos
+        (bx, by) = base
+        if self.pos != self.target:
+            (tx, ty) = self.target
+            if self.ms > abs(x-tx)+abs(y-ty):
+                self.ms = abs(x-tx)+abs(y-ty)
+            if x != tx:
+                x += self.ms if x < tx else -self.ms
+            if y != ty:
+                y += self.ms if y < ty else -self.ms
+            if self.ms < self.maxs:
+                self.ms += 1
+            self.pos = (x, y)
+        else:
+            self.ms = 1
+        if self.sx == 1 and self.sy == 1:
+            gd.pixel(view.win, x + bx, y + by, (255,255,255))
+        else:
+            pygame.draw.rect(view.win, (255,255,255), (bx+x*self.sx,by+y*self.sy,self.sx,self.sy))
+
+class Space:
+    def __init__(self, points, pos = (0,0)):
+        self.pos = pos
+        self.points = points
+        self.divider = None
+        self.folds = []
+            
+    def update(self, view, controller):
+        for p in self.points:
+            p.update(view, self.pos)
+        for f in self.folds:
+            f.update(view, controller)        
+        if self.divider:
+            (dx, dy) = self.divider
+            (x, y) = self.pos
+            if x == 64 and dy > 0:
+                pygame.draw.line(view.win,(120,120,120),(0, y+dy), (1440, y+dy))
+            if y == 34 and dx > 0:
+                pygame.draw.line(view.win,(120,120,120),(x+dx, 0), (x+dx, 960))            
+
+    def fold(self, pos):
+        (x, y) = pos
+        if self.folds:
+            self.folds[0].fold(pos)
+            self.folds[1].fold(pos)
+            return
+        (a, b) = ([], [])
+        maxs = (x+y)//20
+        for p in self.points:
+            (px, py) = p.target
+            if x > 0 and px > x:
+                p.pos = (px-x, py)
+                px = x-abs(px-x)                
+                p.target = (px, py)
+                p.maxs = maxs
+                b.append(p)
+            elif y > 0 and py > y:
+                p.pos = (px, py-y)
+                py = y-abs(py-y)
+                p.target = (px, py)
+                p.maxs = maxs
+                b.append(p)
+            else:
+                a.append(p)
+        (sx, sy) = self.pos
+        if x > 0:
+            x += 1
+        else:
+            y += 1
+        self.folds = [ Space(a, self.pos), Space(b, (x+sx,y+sy)) ]
+        self.points = []
+        self.divider = pos
+
+    def collapse(self):        
+        if self.folds[0].folds:
+            self.folds[0].collapse()
+            self.folds[1].collapse()
+        else:
+            (dx, dy) = self.divider
+            sx = 2 if dx > 0 else 1
+            sy = 2 if dy > 0 else 1
+            self.points = []
+            for p in self.folds[0].points:
+                (px, py) = p.pos
+                p.sx *= sx
+                p.sy *= sy
+                px //= sx
+                py //= sy
+                p.pos = (px, py)
+                p.maxs = 2 if dx > 0 else 1
+                self.points.append(p)
+            for p in self.folds[1].points:
+                (px, py) = p.pos
+                p.sx *= sx
+                p.sy *= sy
+                px = px//sx + dx//p.sx
+                py = py//sy + dy//p.sy
+                p.pos = (px, py)
+                p.maxs = 2 if dx > 0 else 1
+                self.points.append(p)
+            self.divider = None
+            self.folds = []
+        
+
+class Splitter:
+    def __init__(self, space, splits):
+        self.space = space
+        self.splits = splits
+        self.si = 0
+
+    def update(self, view, controller):
+        view.win.fill((0,0,0))
+        self.space.update(view, controller)
+        if view.frame % 30 == 0 and self.si < len(self.splits):
+            self.space.fold(self.splits[self.si])
+            self.si += 1    
+        elif view.frame % 30 == 0 and self.space.folds:
+            self.space.collapse()
+                   
+
+def init(my_dir, controller, font):
+    points = []
+    splits = []
+    maxx = 0
+    maxy = 0
+    with open(my_dir + "/input/day13.txt") as f:
+        for l in f.read().splitlines():
+            if ',' in l:
+                (x,y) = tuple(map(int, l.split(",")))
+                points.append(Point((x,y)))
+                maxx = max(x, maxx)
+                maxy = max(y, maxy)
+            if 'x=' in l:
+                splits.append((int(l.split('=')[1]), 0))
+            if 'y=' in l:
+                splits.append((0, int(l.split('=')[1])))
+        controller.add(Splitter(Space(points, (64,34)), splits))
+    print(maxx,maxy,splits)
+    return splits
+
+view = View(1440,960,30)
+view.setup("Day 13")
+controller = Controller()
+my_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+init(my_dir, controller, view.font)
+view.record(my_dir + "/day13.mp4")
+controller.run(view)
