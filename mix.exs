14cafa8be36856fd6ad72eb5851989a5f574e75e
Staszek Pasko <staszek@gmail.com>
2021-12-15 23:43:01 +0100

    deps

--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule Aoc2021.MixProject do
   defp deps do
     [
       {:benchee, "~> 1.0" },
-      {:priority_queue, "~> 1.0" }
+      {:priority_queue, "~> 1.0" },
+      {:heap, "~> 2.0" }
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
