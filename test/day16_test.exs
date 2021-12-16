4d153a1d6a6f3f2dfeeb2e30561cdbfb918330c8
Staszek Pasko <staszek@gmail.com>
2021-12-16 12:56:35 +0100

    Day 16 tests

--- test/day16_test.exs
+++ test/day16_test.exs
@@ -17,31 +17,25 @@ defmodule Day16Test do
   doctest Aoc2021.Day16
   import Aoc2021.Day16
 
-  def input1, do: [ "D2FE28" ]
-  def input2, do: [ "38006F45291200" ]
-  def input3, do: [ "EE00D40C823060" ]
-  def input4, do: [ "8A004A801A8002F478" ]
-  def input5, do: [ "620080001611562C8802118E34" ]
-  def input6, do: [ "C0015000016115A2E0802F182340" ]
-  def input6, do: [ "A0016C880162017C3686B18A3D4780" ]
-
   test "part 1" do
-    assert part1(input1()) == 6
-    assert part1(input2()) == 1
-    assert part1(input3()) == 14
-    assert part1(input4()) == 16
-    assert part1(input5()) == 12
-    assert part1(input6()) == 23
-    assert part1(input7()) == 31
+    assert part1(["D2FE28"]) == 6
+    assert part1(["38006F45291200"]) == 9
+    assert part1(["EE00D40C823060"]) == 14
+    assert part1(["8A004A801A8002F478"]) == 16
+    assert part1(["620080001611562C8802118E34"]) == 12
+    assert part1(["C0015000016115A2E0802F182340"]) == 23
+    assert part1(["A0016C880162017C3686B18A3D4780"]) == 31
   end
 
   test "part 2" do
-    assert part2(input1()) == 6
-    assert part2(input2()) == 1
-    assert part2(input3()) == 14
-    assert part2(input4()) == 16
-    assert part2(input5()) == 12
-    assert part2(input6()) == 23
-    assert part2(input7()) == 31
+    assert part2(["D2FE28"]) == 2021
+    assert part2(["C200B40A82"]) == 3
+    assert part2(["04005AC33890"]) == 54
+    assert part2(["880086C3E88112"]) == 7
+    assert part2(["CE00C43D881120"]) == 9
+    assert part2(["D8005AC2A8F0"]) == 1
+    assert part2(["F600BC2D8F"]) == 0
+    assert part2(["9C005AC2F8F0"]) == 0
+    assert part2(["9C0141080250320F1802104A08"]) == 1
   end
 end
