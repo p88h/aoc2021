7be50a86534498c4b3052b940bf79444c38a44b8
Staszek Pasko <staszek@gmail.com>
2021-12-20 17:05:49 +0100

    Optimize day20

--- other/day20b.py
+++ other/day20b.py
@@ -1,3 +1,4 @@
+import time
 import itertools
 
 class Board:
@@ -14,19 +15,26 @@ class Board:
         ndflt = self.bitmap[0] if self.dflt == 0 else self.bitmap[-1]
         total = 0
         old = self.board.copy()
-        for (x, y) in itertools.product(range(-step,self.size+step), range(-step, self.size+step)):
-            fval = 0
-            for (dx,dy) in [(-1,-1),(0,-1),(1,-1),(-1,0),(0,0),(1,0),(-1,1),(0,1),(1,1)]:
-                bit = old.get((x+dx,y+dy), self.dflt)
-                fval = fval * 2 + bit
-            nbit = self.bitmap[fval]
-            total += nbit
-            self.board[(x,y)]=nbit
+        self.board={}
+        for x in range(-step,self.size+step):
+            pval = 0
+            for y in range(-step, self.size+step):
+                fval = pval & 63
+                for dx in [-1,0,1]:
+                    bit = old.get((x+dx,y+1), self.dflt)
+                    fval = fval * 2 + bit
+                pval = fval
+                nbit = self.bitmap[fval]
+                total += nbit
+                if nbit != ndflt:
+                    self.board[(x,y)]=nbit
         self.dflt = ndflt
         return total
 
-
+start = time.time()
 lines = open("input/day20.txt").read().splitlines()
 board = Board(lines[0], lines[2:])
 for i in range(1,51):
-    print(i, board.iter(i))
+    t = board.iter(i)
+    end = time.time()
+    print(i, t, end - start)
