f14eb4e7df087f442cb48bf9a2c0e3bead66c436
Staszek Pasko <staszek@gmail.com>
2021-12-21 21:30:18 +0100

    Optimize day21 by disentangling player multiverses

--- lib/day21.ex
+++ lib/day21.ex
@@ -11,22 +11,30 @@ defmodule Aoc2021.Day21 do
 
   def part1(args), do: Enum.map(args, &parse/1) |> iter([0,0],0,0)
 
-  def addstate({{_p1, _p2, s1, _s2}, c}, {ret, w1, w2}) when s1 >= 21, do: { ret, w1 + c, w2 }
-  def addstate({{p1, p2, s1, s2}, c}, {ret, w1, w2}) do
-    { Map.update(ret, {p2, p1, s2, s1}, c, &(&1+c)), w1, w2 }
-  end
+  def addstate({{_p, s}, c}, {ret, [ w | t ]}) when s >= 21, do: { ret, [ w + c | t ] }
+  def addstate({{p, s}, c}, {ret, wins}), do: { Map.update(ret, {p, s}, c, &(&1+c)), wins }
 
-  def produce3({{p1, p2, s1, s2}, c}, {ret, w1, w2}) do
+  def produce3({{p, s}, c}, {ret, wins}) do
     quants = for {d,f} <- [{6, 7},{5, 6},{7, 6},{4, 3},{8, 3},{3, 1},{9, 1}] do
-      np = rem(p1 + d - 1, 10) + 1
-      {{np, p2, s1+np, s2}, c * f}
+      np = rem(p + d - 1, 10) + 1
+      {{np, s+np}, c * f}
     end
-    Enum.reduce(quants, {ret, w1, w2}, &addstate/2)
+    Enum.reduce(quants, {ret, wins}, &addstate/2)
   end
 
-  def iter2([p1, p2]), do: iter2({%{{p1,p2,0,0} => 1}, 0, 0})
-  def iter2({multiverse, win1, win2}) when multiverse == %{}, do: max(win1, win2)
-  def iter2({multiverse, win1, win2}), do: iter2(Enum.reduce(multiverse, {%{}, win2, win1}, &produce3/2 ))
+  def iter2({multiverse, wins}) when multiverse == %{}, do: Enum.reverse(wins)
+  def iter2({multiverse, wins}), do: iter2(Enum.reduce(multiverse, {%{}, [ 0 | wins]}, &produce3/2 ))
 
-  def part2(args), do: Enum.map(args, &parse/1) |> iter2()
+  def compute([], [], _, _, r1, r2), do: max(r1, r2)
+  def compute([w1 | t1], [w2 | t2], s1, s2, r1, r2) do
+    s1 = s1 * 27 - w1
+    r1 = r1 + w1 * s2
+    s2 = s2 * 27 - w2
+    r2 = r2 + w2 * s1
+    compute(t1, t2, s1, s2, r1, r2)
+  end
+
+  def iter3([p1, p2]), do: compute(iter2({%{{p1,0}=> 1}, []}),iter2({%{{p2,0}=> 1}, []}), 1, 1, 0, 0)
+
+  def part2(args), do: Enum.map(args, &parse/1) |> iter3()
 end
--- other/day21b.py
+++ other/day21b.py
@@ -1,10 +1,9 @@
-import re
 from collections import defaultdict
 import time
 
 lines = open("input/day21.txt").readlines()
-p1 = int(re.search(r'Player (\d+) starting position: (\d+)', lines[0]).group(2))
-p2 = int(re.search(r'Player (\d+) starting position: (\d+)', lines[1]).group(2))
+p1 = int(lines[0].split(": ")[1])
+p2 = int(lines[1].split(": ")[1])
 
 
 def run1(p1, p2):
@@ -19,28 +18,39 @@ def run1(p1, p2):
         (p1, p2, s1, s2) = (p2, p1, s2, s1)
 
 
-def produce3(multiverse, p1, p2, s1, s2, c, w, p):
+def produce3(multiverse, p, s, c, w):
     for (d, f) in [(6, 7), (5, 6), (7, 6), (4, 3), (8, 3), (3, 1), (9, 1)]:
-        np = ((p1 + d - 1) % 10) + 1
-        ns = s1 + np
+        np = ((p + d - 1) % 10) + 1
+        ns = s + np
         if ns < 21:
-            multiverse[(p2, np, s2, ns)] += c*f
+            multiverse[(np, ns)] += c*f
         else:
-            w[p] += c*f
+            w[-1] += c*f
 
 
-def run2(p1, p2):
+def run2(p):
     multiverse = defaultdict(int)
-    multiverse[(p1, p2, 0, 0)] = 1
-    wins = [0, 0]
-    player = 0
+    multiverse[(p, 0)] = 1
+    wins = []
     while multiverse:
+        wins.append(0)
         temp = defaultdict(int)
-        for (p1, p2, s1, s2) in multiverse:
-            produce3(temp, p1, p2, s1, s2, multiverse[(p1, p2, s1, s2)], wins, player)
-        player = 1-player
-        multiverse = temp
-    return max(wins)
+        for (p, s) in multiverse:
+            produce3(temp, p, s, multiverse[(p, s)], wins)
+        multiverse = temp        
+    return wins
+
+def run3(p1,p2):
+    wins1 = run2(p1)
+    wins2 = run2(p2)
+    size1 = size2 = 1
+    w1 = w2 = 0
+    for step in range(len(wins1)):
+        size1 = size1 * 27 - wins1[step]
+        w1 += wins1[step]*size2
+        size2 = size2 * 27 - wins2[step]
+        w2 += wins2[step]*size1
+    return max(w1,w2)
 
 
 start = time.time()
@@ -48,6 +58,6 @@ ret1 = run1(p1, p2)
 end = time.time()
 print(ret1, end-start)
 start = time.time()
-ret2 = run2(p1, p2)
+ret2 = run3(p1, p2)
 end = time.time()
 print(ret2, end-start)
